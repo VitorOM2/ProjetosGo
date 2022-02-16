929182b7eed6b7e771b8f5fe7009f8f0732991c8
VitorOM2 <viktormarque2053@gmail.com>
2022-02-15 22:45:12 -0300

    Adicionados exemplos de dados booleans

--- Datatypes/Tipos.go
+++ Datatypes/Tipos.go
@@ -8,4 +8,11 @@ func main() {
 	fmt.Println("Tipos de dados: Boolean, tipos númericos e tipos de textos")
 
 	// ---------- BOOLEAN ----------
+	fmt.Println("Boolean: Verdadeiro ou falso")
+
+	var bP bool = true
+	var bN bool = false
+
+	fmt.Printf("Exemplo de um boolean positivo: %v, %T \n", bP, bP)
+	fmt.Printf("Exemplo de um boolean falso: %v, %T \n", bN, bN)
 }
