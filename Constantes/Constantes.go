6915e55a1f8035e89733fd476b7df037ae511c70
VitorOM2 <viktormarque2053@gmail.com>
2022-03-02 22:32:29 -0300

    Adicionado exemplo de iota

--- Constantes/Constantes.go
+++ Constantes/Constantes.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 )
 
+const (
+	// Iota auto incrementa constantes
+	a = iota // 0
+	b = iota // 1
+	c = iota // 2
+)
+
 func main() {
-	const minha_const int = 8 // Exemplo de declaração de constante
-	fmt.Println(minha_const)
+	// const minha_const int = 8 // Exemplo de declaração de constante
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
 }
