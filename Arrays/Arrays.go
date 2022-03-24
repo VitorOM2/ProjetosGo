0672ecc51ec53924200f5d59e92d7dfa688c8974
VitorOM2 <viktormarque2053@gmail.com>
2022-03-23 22:16:56 -0300

    Adicionado mais comentários

--- Arrays/Arrays.go
+++ Arrays/Arrays.go
@@ -11,11 +11,11 @@ func main() {
 	notas := [...]int{8, 8, 10}
 
 	// ----- Mostra no console -----
-	fmt.Printf("Alunos %v \n", alunos)
+	fmt.Printf("Alunos %v \n", alunos) // Array sem nenhum item
 	fmt.Printf("Notas: %v \n", notas)
-	alunos[0] = "João"
+	alunos[0] = "João" // Adiciona um item para a array
 	alunos[1] = "Maria"
 	alunos[2] = "Ana"
-	fmt.Printf("Alunos %v \n", alunos)
+	fmt.Printf("Alunos %v \n", alunos)                  // Array atualizada com itens
 	fmt.Printf("Quantidade de alunos: %v", len(alunos)) //Mostra a quantidade de itens em um array
 }
