0cfda0a2e759fcc410bef0c4f93177134b4810f0
VitorOM2 <viktormarque2053@gmail.com>
2022-03-23 22:28:08 -0300

    Criado arrays dentro de outra array

--- Arrays/Arrays.go
+++ Arrays/Arrays.go
@@ -11,11 +11,17 @@ func main() {
 	notas := [...]int{8, 8, 10}
 
 	// ----- Mostra no console -----
+
+	// 1# Primeiro exemplo
 	fmt.Printf("Alunos %v \n", alunos) // Array sem nenhum item
 	fmt.Printf("Notas: %v \n", notas)
 	alunos[0] = "João" // Adiciona um item para a array
 	alunos[1] = "Maria"
 	alunos[2] = "Ana"
-	fmt.Printf("Alunos %v \n", alunos)                  // Array atualizada com itens
-	fmt.Printf("Quantidade de alunos: %v", len(alunos)) //Mostra a quantidade de itens em um array
+	fmt.Printf("Alunos %v \n", alunos)                    // Array atualizada com itens
+	fmt.Printf("Quantidade de alunos: %v\n", len(alunos)) //Mostra a quantidade de itens em um array
+
+	// 2# Segundo exemplo
+	var superArray [3][3]int = [3][3]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
+	fmt.Println(superArray)
 }
