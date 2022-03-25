590e80e6c9cc484a8db6988b77f0d4d78c0fde9b
VitorOM2 <viktormarque2053@gmail.com>
2022-03-24 22:19:16 -0300

    Criado um segundo slice

--- Slices/Slice.go
+++ Slices/Slice.go
@@ -6,7 +6,10 @@ import (
 
 func main() {
 	a := []int{1, 2, 3}
-	fmt.Println(a)
+	b := a         // Cria um segundo slice que está apontando para o primeiro
+	b[1] = 5       // O segundo slice modifica o segundo indice dos slices
+	fmt.Println(a) // Slice modificado por causa do segundo slice
+	fmt.Println(b)
 	fmt.Printf("Tamanho:	%v \n", len(a)) // Mostra o tamanho do slice
 	fmt.Printf("Capacidade:	%v", cap(a)) // Mostra a capacidade do slice
 }
