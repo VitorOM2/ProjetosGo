31292c5c68243c4c90f737a6cda0b4fe4811233a
VitorOM2 <viktormarque2053@gmail.com>
2022-04-11 18:45:38 -0300

    Adicionado segundo exemplo

--- Structs/Structs.go
+++ Structs/Structs.go
@@ -4,14 +4,29 @@ import (
 	"fmt"
 )
 
-// Struct
+// Structs
+// Exemplo 1
 type Medico struct {
 	numMed  int
 	nomeMed string
 	comp    []string
 }
 
+//Exemplo 2
+type Animal struct {
+	Nome   string
+	Origem string
+}
+
+type Passaro struct {
+	Animal
+	VelocidadeKPH float32
+	PodeVoar      bool
+}
+
 func main() {
+
+	// ----- Exemplo 1 -----
 	umMedico := Medico{
 		numMed:  8,
 		nomeMed: "João",
@@ -20,5 +35,14 @@ func main() {
 			"Paulo",
 		},
 	}
+	// ---------------------
+	// ----- Exemplo 2 -----
+	p := Passaro{}
+	p.Nome = "Emu"
+	p.Origem = "Austrália"
+	p.VelocidadeKPH = 48
+	p.PodeVoar = false
+
 	fmt.Println(umMedico)
+	fmt.Println(p)
 }
